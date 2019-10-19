7e83b974e9e6560db69834239e030f0d642dd1ea
Samuel Macleod <samuel@macleod.space>
2019-10-19 02:37:23 +0100

    Async/await ify all the things

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express        = require('express')
 const scrape_members = require('./scrape.js')
 const fs             = require('fs')
+const fs_async       = require('fs').promises
 const { DateTime }   = require('luxon')
 
 const readJSON = file => JSON.parse(fs.readFileSync(file))
@@ -30,30 +31,33 @@ const authenticationMiddleware = (req, res, next) => {
 app.use(authenticationMiddleware)
 
 const writeScrape = async () => {
-    const members = await scrape_members({orgID, groupID, auth: secrets})
+    try {
+        const members = await scrape_members({orgID, groupID, auth: secrets})
 
-    const out = {
-        members: members,
-        date: new Date().toISOString()
+        const out = {
+            members: members,
+            date: new Date().toISOString()
+        }
+
+        await fs_async.writeFile(
+            cachefile,
+            JSON.stringify(out)
+        )
+
+        return out
+    } catch(e) {
+        console.error(e)
     }
-
-    fs.writeFileSync(
-        cachefile,
-        JSON.stringify(out)
-    )
-
-    return out
 }
 
-const readScrape = () => JSON.parse(fs.readFileSync(cachefile))
+const readScrape = async () => JSON.parse(await fs_async.readFile(cachefile))
 
-app.get('/api/members', (req, res) => {
-    res.json({ success: true, ...readScrape()})
+app.get('/api/members', async (req, res) => {
+    res.json({ success: true, ...(await readScrape())})
 })
 
-app.get('/api/refresh', (req, res) => {
-    writeScrape()
-        .then(r => res.json({ success: true, ...r}))
+app.get('/api/refresh', async (req, res) => {
+    res.json({ success: true, ...(await writeScrape())})
 })
 
 
